6ecb5cb799d2700513c382bf16171c024df57ab9
Isac Wälimaa <isac.walimaa@tietoevry.com>
2024-04-26 08:42:06 +0200

    init2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { useState } from "react";
 
 export default function Page() {
@@ -12,54 +11,67 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
+      "Nej",
+      "Är du säker?",
+      "Om jag frågar snällt?",
+      "Snälla <3",
       ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  const handleYesClick = () => {
+    fetch('https://phonefunctionapp.azurewebsites.net/api/Function1', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        shouldSendSMS: true
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Response from Azure Function:', data);
+      // Display the "WOOOO" message
+    })
+    .catch(error => {
+      console.error('Error calling Azure Function:', error);
+    });
+  
+    // Set yesPressed to true regardless of the fetch result
+    setYesPressed(true);
   };
 
   return (
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="bear gif" />
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! ;))</div>
         </>
       ) : (
         <>
           <img
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            alt="love bear gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Vill du ta en öl med mig?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
               style={{ fontSize: yesButtonSize }}
-              onClick={() => setYesPressed(true)}
+              onClick={handleYesClick} // Call handleYesClick instead of setYesPressed(true)
             >
-              Yes
+              Ja
             </button>
             <button
               onClick={handleNoClick}
               className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
             >
-              {noCount === 0 ? "No" : getNoButtonText()}
+              {noCount === 0 ? "Nej" : getNoButtonText()}
             </button>
           </div>
         </>
